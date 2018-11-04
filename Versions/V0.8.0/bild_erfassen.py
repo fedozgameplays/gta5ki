05a9be0579d8c7cf9a52733f9267ea160689e0e6
fedozgameplays <fedozgameplays@yahoo.com>
2018-11-04 11:21:04 +0100

    optimierungen

---
+++ Versions/V0.8.0/bild_erfassen.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import win32gui
+import win32ui
+import win32con
+
+
+def grab_screen(region=None):
+
+    hwin = win32gui.FindWindow(None, "Grand Theft Auto V")
+    rect = win32gui.GetWindowRect(hwin)
+
+    x = rect[0]
+    y = rect[1]
+    left = 0
+    top = 40
+    height = rect[3] - y - top
+    width = rect[2] - x
+
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype="uint8")
+    img.shape = (height, width, 4)
+
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
