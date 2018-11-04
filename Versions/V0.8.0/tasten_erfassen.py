c640e5f128f8b6c7f27cb0c206c77576f267e77b
fedozgameplays <fedozgameplays@yahoo.com>
2018-11-04 11:22:06 +0100

    V0.8.0

---
+++ Versions/V0.8.0/tasten_erfassen.py
@@ -0,0 +1,13 @@
+import win32api
+
+keyList = ["\b"]
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
+    keyList.append(char)
+
+
+def key_check():
+    keys = []
+    for key in keyList:
+        if win32api.GetAsyncKeyState(ord(key)):
+            keys.append(key)
+    return keys
